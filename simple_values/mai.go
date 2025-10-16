ab82dc4bdac5e7876963697883d93b7b2ab6b809
Aditya Aryan <adityaaryan531@gmail.com>
2025-10-16 12:25:11 +0530

    variables in go

---
+++ simple_values/mai.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Mai!")
+	fmt.Println(2)
+	fmt.Println(true)
+}
---
+++ variables/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	var name string = "Golang"
+	// or can do this
+	var name1 = "Golang"
+	var age = 10
+	var isAwesome = true
+
+	// shorthand syntax
+	// name := "Golang"
+	// age := 10
+	// isAwesome := true
+
+	println("Name:", name)
+	println("Age:", age)
+	println("Is Awesome:", isAwesome)
+	println("Name1:", name1)
+}
